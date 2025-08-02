40d43658e7765c64c34a94b734222e01256b489f
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-08-02 21:34:32 +0530

    Added a custom youtube loader that uses yt-dlp in the backend which is better than pytube

--- loader.py
+++ loader.py
@@ -1,18 +1,19 @@
 from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
-
+from youtube_loader import load_youtube_content
 
 
 def data_loader(url):
 
     if "youtube.com" in url or "youtu.be" in url:
-        loader = YoutubeLoader.from_youtube_url(url,add_video_info = True)
+        docs =load_youtube_content(url)
+        return docs
     else:
-        loader = UnstructuredURLLoader(urls = [url], ssl_verify = False,
+        loader = UnstructuredURLLoader(urls = [url], 
+                                       ssl_verify = False,
                                     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
                     
-    docs = loader.load()
-
-    return docs
+        docs = loader.load()
+        return docs
         
     
 
