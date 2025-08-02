b57350dbf0d857be7f7ffa514645a67feb9990ef
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-08-03 01:00:04 +0530

    Removed youtube, added option for summarization technique

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ load_dotenv()
 
 
 from loader import data_loader
-from config import llm_model, prompt
+from config import llm_model, stuff_prompt, map_prompt, combine_prompt
 
 
 
-st.set_page_config(page_title="Youtube and Website Text Summarizer")
-st.title("LangChain-Powered Web and Youtube Summarizer")
+st.set_page_config(page_title="Website Text Summarizer")
+st.title("LangChain-Powered Web Summarizer")
 st.subheader("Provide a URL to summarize the content within seconds !")
 
 
@@ -26,12 +26,19 @@ api_key = os.getenv("GROQ_API_KEY")
 
 url = st.text_input("URL", label_visibility="collapsed")
 
+summarization_method = st.selectbox(
+    "Choose summarization method for long content:",
+    ["Map-Reduce", "Stuff"]
+)
+
+
+
+
 if st.button("Summarize the content"):
     if not url.strip():
         st.error("Please enter a URL to get started")
     elif not validators.url(url):
         st.error("Please enter a valid URL")
-        st.info("URL may be a YouTube video or a website url")
 
     else:
         try:
@@ -39,12 +46,32 @@ if st.button("Summarize the content"):
                 ##loading the data
                 docs = data_loader(url)
                 #chain
-                chain = load_summarize_chain(llm= llm_model,
-                                             chain_type="stuff",
-                                             prompt=prompt)
-                output_summary = chain.run(docs)
-                
-                st.success(output_summary)
+                if summarization_method == "Stuff":
+                    chain_type = "stuff"
+
+                elif summarization_method == "Map-Reduce":
+                    chain_type = "map_reduce"
+
+                #summary chain declaration
+                with st.spinner("Just a moment..."):
+
+                    with st.spinner("Calibrating the chain type..."):
+
+                        if chain_type=="stuff":
+                            chain = load_summarize_chain(llm= llm_model,
+                                                        chain_type="stuff",
+                                                        prompt=stuff_prompt)
+                        else:
+                            chain = load_summarize_chain(llm= llm_model,
+                                                        chain_type="map_reduce",
+                                                        map_prompt=map_prompt,
+                                                        combine_prompt= combine_prompt,
+                                                        verbose = True)
+                     
+                    output_summary = chain.run(docs)
+                    
+                    st.success("Your summary is successfully generated ! Take a look...")
+                    st.write(output_summary)
 
         except Exception as e:
             st.exception(f"Exception: {e}")
--- loader.py
+++ loader.py
@@ -1,19 +1,20 @@
 from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
-from youtube_loader import load_youtube_content
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def data_loader(url):
 
-    if "youtube.com" in url or "youtu.be" in url:
-        docs =load_youtube_content(url)
-        return docs
-    else:
-        loader = UnstructuredURLLoader(urls = [url], 
-                                       ssl_verify = False,
-                                    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
-                    
-        docs = loader.load()
-        return docs
+    loader = UnstructuredURLLoader(
+                                urls = [url], 
+                                ssl_verify = False,
+                                headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})           
+    docs = loader.load()
+
+    text_splitter =  RecursiveCharacterTextSplitter(chunk_size = 1500, chunk_overlap = 200)
+    chunks = text_splitter.split_documents(docs)
+
+    
+    return chunks
         
     
 
